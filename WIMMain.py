20be483cef10dc09dd429ee59d408a98d9e56632
Qmay654 <smeff12@gmail.com>
2023-10-11 10:36:12 -0400

    Spelling mistake :(

--- WIMMain.py
+++ WIMMain.py
@@ -20,9 +20,9 @@ java_args = [
 subprocess.Popen(java_args)
 
 # Create a socket to listen for connections from the Java GUI
-java_reciever_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-java_reciever_socket.bind(('localhost', 12345))
-java_reciever_socket.listen(1)
+java_receiver_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+java_receiver_socket.bind(('localhost', 12345))
+java_receiver_socket.listen(1)
 
 connected = True
 while connected:
@@ -30,7 +30,7 @@ while connected:
     print("Waiting for Java GUI to send a message...")
     
     # Accept a connection from the Java GUI
-    java_sender_socket, address = java_reciever_socket.accept()
+    java_sender_socket, address = java_receiver_socket.accept()
     print(f"Connected to {address}")
 
     # Receive user input from the Java GUI
@@ -48,8 +48,8 @@ while connected:
     java_sender_socket.send(result.encode())
     print("Message sent to Java GUI\n")
 
+    # Close the sender socket
+    java_sender_socket.close()
 
-# Close the sender socket
-java_sender_socket.close()
 # Close the receiver socket
-java_reciever_socket.close()
+java_receiver_socket.close()
