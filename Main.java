52888a0fb84bfd24cb29361fc5e8877d1a4de936
Qmay654 <smeff12@gmail.com>
2023-10-11 00:29:31 -0400

    Initial commit

---
+++ Main.java
@@ -0,0 +1,147 @@
+package runable;
+
+import java.util.Date;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.scene.text.TextFlow;
+import javafx.stage.Stage;
+
+public class Main extends Application implements Initializable {
+
+	@FXML
+	TextField enterName;
+	@FXML
+	TextField enterText;
+	@FXML
+	TextFlow displayText;
+	@FXML
+	ScrollPane scrollPane;
+	@FXML
+	Button exitButton;
+	String name;
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		Parent p = FXMLLoader.load(getClass().getResource("WIM.v.2.fxml"));
+
+		Scene root = new Scene(p);
+		primaryStage.setScene(root);
+		primaryStage.setTitle("WIM: Wentworth Instant Messaging");
+		primaryStage.getIcons().add(new Image("images/e35495597930766.png"));
+		primaryStage.setResizable(false);
+		primaryStage.show();
+
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+
+		enterName.setOnKeyPressed((e) -> {
+
+			if (e.getCode().equals(KeyCode.ENTER)) {
+				name = enterName.getText();
+				if (name != null) { // Make sure they entered a name
+					if (name.length() < 16) { // Name is <= 15 characters
+						enterName.setDisable(true);
+					} else { // Error message for name being too long
+						Text t = new Text(String.format("Please make your screen name less than 15 characters%n"));
+						t.setFill(Color.RED);
+						displayText.getChildren().add(t);
+						name = null;
+					}
+				}
+			}
+
+		});
+
+		enterText.setOnKeyPressed((e) -> {
+
+			if (e.getCode().equals(KeyCode.ENTER)) {
+				String message = enterText.getText();
+				if (message != "") {
+					if (name != null) { // Must have a screen name set before sending a message
+						enterText.clear();
+						// Create a socket to connect to the Python script
+						Socket socket;
+
+						try {
+							// Create a socket to connect to the Python script
+							socket = new Socket("localhost", 12345);
+
+							// Send the user input to the Python script
+							PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+							out.println(message);
+
+							// Receive the result from the Python script
+							BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+							String result = in.readLine();
+
+							// Display the message from the Python script
+							displayText.getChildren()
+									.add(new Text(String.format("[%s] %s: %s%n", getTime(), name, result)));
+
+							// Close the socket
+							socket.close();
+
+						} catch (IOException e1) {
+							e1.printStackTrace();
+						}
+
+					} else { // Error message for not having a screen name
+						Text t = new Text(String.format("Please enter a screen name before messaging%n"));
+						t.setFill(Color.RED);
+						displayText.getChildren().add(t);
+					}
+				}
+			}
+		});
+
+		exitButton.setOnAction((e) -> {
+			try {
+				Socket socket = new Socket("localhost", 12345);
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				out.println("EXIT");
+				socket.close();
+				System.exit(0);
+			} catch (IOException e1) {
+			}
+		});
+
+	}
+
+	/**
+	 * Method to return the current time in HH:MM:SS format
+	 * 
+	 * @return string time in HH:MM:SS format
+	 */
+	public static String getTime() {
+		String date = new Date().toString();
+		String[] s = date.split(" ");
+
+		return s[3];
+
+	}
+}
---
+++ Testing.py
@@ -0,0 +1,55 @@
+import socket
+import subprocess
+
+exit_message = "EXIT"
+
+java_gui_jar_path = "WIM.jar"
+
+# Specify the Java module path and modules as separate arguments
+java_args = [
+    "java",
+    "--module-path",
+    "C:/Users/Quentyn May/Documents/javafx-sdk-19.0.2.1/lib",
+    "--add-modules",
+    "javafx.controls,javafx.fxml",
+    "-jar",
+    java_gui_jar_path,
+]
+
+# Run the Java GUI using subprocess
+subprocess.Popen(java_args)
+
+# Create a socket to listen for connections from the Java GUI
+java_reciever_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+java_reciever_socket.bind(('localhost', 12345))
+java_reciever_socket.listen(1)
+
+connected = True
+while connected:
+    
+    print("Waiting for Java GUI to send a message...")
+    
+    # Accept a connection from the Java GUI
+    java_sender_socket, address = java_reciever_socket.accept()
+    print(f"Connected to {address}")
+
+    # Receive user input from the Java GUI
+    user_input = java_sender_socket.recv(2048).decode('utf-8')
+    print("Message recieved from Java GUI")
+    
+    if user_input.strip() == exit_message:
+        connected = False
+        break
+        
+    # Stores result
+    result = f"{user_input}"
+
+    # Send the result back to the Java GUI
+    java_sender_socket.send(result.encode())
+    print("Message sent to Java GUI\n")
+
+
+# Close the sender socket
+java_sender_socket.close()
+# Close the receiver socket
+java_reciever_socket.close()
