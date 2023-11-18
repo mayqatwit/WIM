8b9d65168a9c3d8b3fdd457de5379d60e7e117ce
Qmay654 <smeff12@gmail.com>
2023-11-18 13:45:36 -0500

    updated the jar file

--- WIMClient.py
+++ WIMClient.py
@@ -90,7 +90,7 @@ def send_message(message):
             print(user[2], user[1])
             send.connect((user[2], user[1]))
             send.sendall(message.encode())
-            # send.sendall(user[0].encode())
+            send.sendall(user[0].encode())
             print("Message sent to user")
             send.close()  # Close the socket after sending the message
 
@@ -98,18 +98,16 @@ def send_message(message):
 def handle_client(client, addr, java_sender_port):
     users = request_users()
     print("Handling")
-    other_name = ''
-    for user in users:
-        if user[2] == addr[0]:
-            other_name = user[0]
-            continue
+    # for user in users:
+    #     if user[2] == addr[0]:
+    #         other_name = user[0]
+    #         continue
 
     # Accept the message being sent
     incoming_message = client.recv(2048).decode(ENCODE).strip()
-    # other_name = client.recv(2048).decode(ENCODE).strip()
+    other_name = client.recv(2048).decode(ENCODE).strip()
     if incoming_message != exit_message:
         print(incoming_message)
-
 
         print(other_name, ":", incoming_message)
 
